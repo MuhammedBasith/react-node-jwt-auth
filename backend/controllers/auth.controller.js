6415821c10a2eb3b79c8d3d65734d55da5c57c9a
MuhammedBasith <iambasith123@gmail.com>
2025-01-19 02:04:24 +0530

    add routes

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,31 @@
+import { User } from "../models/user.model"
+import bcrypt from 'bcryptjs'
+
 export const signup = async (req, res) => {
-    res.send("Signup Route")
+    const { email, password, name } = req.body
+
+    try {
+        if (!email || !password || !name)
+            throw new Error("All fields are required")
+
+        const userAlreadyExists = await User.findOne({ email })
+        if (userAlreadyExists) {
+            return res.status(400).json({success: false, message: "User already exists"})
+        }
+
+        const hashedMessage = await bcrypt.hash(password, 10)
+        const user = new User({
+            email, 
+            password: hashedMessage,
+            name
+        })
+
+
+        res.send("Signup Route")
+    }catch(error){
+        return res.status(400).json({success: false, message: error.message})
+
+    }
 }
 
 export const login = async (req, res) => {
